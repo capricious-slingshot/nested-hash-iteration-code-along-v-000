675cf563486cb342935e3ce52e428a6102900542
Carden Z <ephemere@protonmail.com>
2019-02-02 04:53:42 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |person, details_hash|
     details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
-        data.delete_if {|ice cream| ice_cream == "strawberry"}
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
       end
     end
 
